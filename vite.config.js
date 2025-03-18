9c8587f2f48e6e1a2fe23ea92a4d2b17f363f26a
Serge Iangicher <sergey.yangicher@rockyroad.io>
2025-03-18 10:34:10 +0200

    change deploy

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,7 @@
 import { defineConfig } from 'vite';
-const { glob } = await import("glob");
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
-
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
-      },
-      outDir: '../dist',
-      emptyOutDir: true,
-    },
-    plugins: [
-      injectHTML({
-        injectData: {
-          partials: '/src/partials/'  // 👈 Correct path to your partials folder
-        }
-      }),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+export default defineConfig({
+  build: {
+    outDir: 'dist' // Ensure this is correctly set
+  }
 });
